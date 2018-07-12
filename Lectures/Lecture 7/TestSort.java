cdfb9a190b10ccc3cecf056baf2a3ffcc4455c7b
timzyt <yitianzhu@gmail.com>
2018-07-11 22:55:04 -0700

    commit 1st version Lecture 7 live code

---
+++ Lectures/Lecture 7/TestSort.java
@@ -0,0 +1,23 @@
+/** Tests the sort class. */
+
+public class TestSort{
+	/** Test the Sort.sort method. */
+	public static void testSort() {
+		String[] input = {"i", "have", "an", "egg"};
+		String[] expected = {"an", "egg", "have", "i"}; 
+
+		Sort.sort(input);
+
+		for (int i = 0; i < input.length; i+=1) {
+			if (!input[i].equals(expected[i])) {
+				System.out.println("Mismatch in position " + i + ", expected: " + expected[i] + ", but got: " + input[i]); 
+				return;
+			}
+		}
+			
+	}
+
+	public static void main(String[] args) {
+		testSort();
+	}
+}
